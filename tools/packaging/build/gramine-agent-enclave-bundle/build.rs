24d859b66298834310a1416319aa28bb71ec6563
Mikko Ylinen <mikko.ylinen@intel.com>
2023-04-28 14:00:38 +0300

    WIP: Update Gramine support with the fixes included
    Signed-off-by: Mikko Ylinen <mikko.ylinen@intel.com>

--- tools/packaging/build/gramine-agent-enclave-bundle/build.rs
+++ tools/packaging/build/gramine-agent-enclave-bundle/build.rs
@@ -4,6 +4,9 @@
 //
 
 fn main() -> shadow_rs::SdResult<()> {
+    println!("cargo:rustc-link-search=/usr/lib/x86_64-linux-gnu/");
+    println!("cargo:rustc-link-lib=static=sgx_util");
+    println!("cargo:rustc-link-lib=static=mbedcrypto_gramine");
     tonic_build::compile_protos("./protos/getresource.proto")?;
 
     shadow_rs::new()
